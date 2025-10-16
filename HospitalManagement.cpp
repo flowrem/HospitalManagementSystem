68a7fe6d4e4e313065e79a735c265f3ae2be24bb
flowrem <mendozaac@students.nu-lipa.edu.ph>
2025-10-16 23:56:48 +0800

    Added methods, but still empty.

--- HospitalManagement.cpp
+++ HospitalManagement.cpp
@@ -1,4 +1,46 @@
-using namespace std;
 #include <iostream>
 #include <string>
+using namespace std;
 
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* prev;
+
+        Node(int value){
+            data = value;
+            next = nullptr;
+            prev = nullptr;
+        }
+
+};
+
+class HospitalManagement {
+    Node* head;
+    Node* tail;
+
+    public:
+        HospitalManagement() {
+            head = nullptr;
+            tail = nullptr;
+        }
+        void manageDoctors(){
+
+        }
+        void admitPatients(){
+            
+        }
+        void undoLastAdmission(){
+
+        }
+        
+        void displayChoices(){
+
+        }
+};
+
+int main(){
+
+    return 0;
+}
